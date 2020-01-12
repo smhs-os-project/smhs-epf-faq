08596a01ab6ac993184da7e45311585c678f7cb0
pan93412 <pan93412@gmail.com>
2020-01-12 20:41:49 +0800

    doc almost done

--- src/pages.js
+++ src/pages.js
@@ -3,6 +3,8 @@ module.exports = {
     'index', // Homepage
     'epf/login', // 學習歷程檔案 - 登入
     'epf/upload', // 學習歷程檔案 - 上傳
-    'epf/request' // 學習歷程檔案 - 認證
+    'epf/request', // 學習歷程檔案 - 認證
+    'doc/compress_pdf', // 文件掃描／轉檔 - 壓縮 PDF
+    'doc/convert' // 文件掃描／轉檔 - Word/PPT 轉檔
   ]
 }
