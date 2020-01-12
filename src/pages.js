4587ac2c08f41e50623a0678510383504c76809b
pan93412 <pan93412@gmail.com>
2020-01-12 21:13:51 +0800

    snapshot 2020-01-12-21-13

--- src/pages.js
+++ src/pages.js
@@ -1,10 +1,12 @@
 module.exports = {
   pages: [
     'index', // Homepage
+    'about', // About
     'epf/login', // 學習歷程檔案 - 登入
     'epf/upload', // 學習歷程檔案 - 上傳
     'epf/request', // 學習歷程檔案 - 認證
     'doc/compress_pdf', // 文件掃描／轉檔 - 壓縮 PDF
-    'doc/convert' // 文件掃描／轉檔 - Word/PPT 轉檔
+    'doc/convert', // 文件掃描／轉檔 - Word/PPT 轉檔
+    'doc/scan' // 文件掃描／轉檔 - 文件掃描
   ]
 }
