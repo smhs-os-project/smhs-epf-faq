e3293231c0b85f9c55e627ef5a0e23b010939a8a
pan93412 <pan93412@gmail.com>
2020-01-13 19:40:37 +0800

    feat(pages): now "compress_img.pug" will be bulit
    NOTICE: this commit is unbuildable,
    we didn't add compress_img.pug to
    this repository.

--- src/pages.js
+++ src/pages.js
@@ -6,6 +6,7 @@ module.exports = {
     'epf/upload', // 學習歷程檔案 - 上傳
     'epf/request', // 學習歷程檔案 - 認證
     'doc/compress_pdf', // 文件掃描／轉檔 - 壓縮 PDF
+    'doc/compress_img', // 文件掃描／轉檔 - 壓縮圖片
     'doc/convert', // 文件掃描／轉檔 - Word/PPT 轉檔
     'doc/scan' // 文件掃描／轉檔 - 文件掃描
   ]
