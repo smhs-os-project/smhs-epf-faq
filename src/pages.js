1f84755d19c45ebe49d893878b506cfd8baa4d5e
pan93412 <pan93412@gmail.com>
2020-01-14 22:52:56 +0800

    feat(pages): add 'epf/explain'
    notice: this commit is unbuildable
    since 'explain.pug' is still not uploaded

--- src/pages.js
+++ src/pages.js
@@ -5,6 +5,7 @@ module.exports = {
     'epf/login', // 學習歷程檔案 - 登入
     'epf/upload', // 學習歷程檔案 - 上傳
     'epf/request', // 學習歷程檔案 - 認證
+    'epf/explain', // 多元表現說明
     'doc/compress_pdf', // 文件掃描／轉檔 - 壓縮 PDF
     'doc/compress_img', // 文件掃描／轉檔 - 壓縮圖片
     'doc/concatenate_imgs', // 文件掃描／轉檔 - 合併多張圖片
