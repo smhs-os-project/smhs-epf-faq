4474447b759269766a87c882983b4012b63cf2a6
pan93412 <pan93412@gmail.com>
2020-01-17 13:30:39 +0800

    fix(js/js): make sure that all the DOM elements are loaded

--- src/js/js.js
+++ src/js/js.js
@@ -28,43 +28,47 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 // includes/header.pug: top button
-$$('.back-top').forEach(e => e.addEventListener('click', () => {
-  document.documentElement.scrollTop = 0
-  document.body.scrollTop = 0
-}))
+document.addEventListener('DOMContentLoaded', () => {
+  $$('.back-top').forEach(e => e.addEventListener('click', () => {
+    document.documentElement.scrollTop = 0
+    document.body.scrollTop = 0
+  }))
+})
 
 // includes/header.pug: countdown
 // thanks for https://www.w3schools.com/howto/howto_js_countdown.asp
-const countdown = $$('.countdown')
-const countdownInterval = setInterval(_ => {
-  if (countdown == null) clearInterval(countdownInterval)
-  countdown.forEach(e => {
-    const expiredTime = new Date(e.dataset.expired).getTime()
-    const now = new Date().getTime()
-    const distance = expiredTime - now
+document.addEventListener('DOMContentLoaded', () => {
+  const countdown = $$('.countdown')
+  const countdownInterval = setInterval(_ => {
+    if (countdown == null) clearInterval(countdownInterval)
+    countdown.forEach(e => {
+      const expiredTime = new Date(e.dataset.expired).getTime()
+      const now = new Date().getTime()
+      const distance = expiredTime - now
 
-    if (distance > 0) {
-      const days = Math.floor(distance / (1000 * 60 * 60 * 24))
-      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
-      const seconds = Math.floor((distance % (1000 * 60)) / 1000)
+      if (distance > 0) {
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24))
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000)
 
-      e.innerHTML = `剩餘${days}日${hours}時${minutes}分${seconds}秒`
-    } else {
-      clearInterval(countdownInterval)
-      e.innerHTML = '時間到！'
-    }
-  })
-}, 1000)
+        e.innerHTML = `剩餘${days}日${hours}時${minutes}分${seconds}秒`
+      } else {
+        clearInterval(countdownInterval)
+        e.innerHTML = '時間到！'
+      }
+    })
+  }, 1000)
+})
 
 // views: 自動設定 target='_blank'
-$$('a.is-blank').forEach(e => e.setAttribute('target', '_blank'))
+document.addEventListener('DOMContentLoaded', () => $$('a.is-blank').forEach(e => e.setAttribute('target', '_blank')))
 
 /**
  * Clean up all the specified track queries.
  * @author pan93412
  */
-(function (queries = []) {
+document.addEventListener('DOMContentLoaded', () => (function (queries = []) {
   const url = (new URL(location.href))
   const urlParams = url.searchParams
   let isCleaned = 0
@@ -78,4 +82,4 @@ $$('a.is-blank').forEach(e => e.setAttribute('target', '_blank'))
     console.warn('We detected track queries! - removed.')
     history.pushState({}, '', url.href)
   }
-})(['fbclid'])
+})(['fbclid']))
