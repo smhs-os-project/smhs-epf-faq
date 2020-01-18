f025883db41172f4c222dbee4520e8991118912d
pan93412 <pan93412@gmail.com>
2020-01-18 14:42:29 +0800

    feat(js/countdown): update scripts to match the new layout

--- src/js/countdown.js
+++ src/js/countdown.js
@@ -3,25 +3,27 @@ const { $$ } = require('./libs/query.js')
 // includes/countdown.pug: countdown
 // thanks for https://www.w3schools.com/howto/howto_js_countdown.asp
 document.addEventListener('DOMContentLoaded', () => {
-  const countdown = $$('.countdown')
+  const countdownTimer = {
+    days: $$('.countdown > span.days > .num')[0],
+    hours: $$('.countdown > span.hours > .num')[0],
+    minutes: $$('.countdown > span.minutes > .num')[0],
+    seconds: $$('.countdown > span.seconds > .num')[0]
+  }
+  const setTimer = (range, num) => { countdownTimer[range].innerHTML = num }
+  const expiredTime = new Date($$('.countdown')[0].dataset.expired).getTime()
+
   const countdownInterval = setInterval(_ => {
-    if (countdown == null) clearInterval(countdownInterval)
-    countdown.forEach(e => {
-      const expiredTime = new Date(e.dataset.expired).getTime()
-      const now = new Date().getTime()
-      const distance = expiredTime - now
+    const now = new Date().getTime()
+    const distance = expiredTime - now
 
-      if (distance > 0) {
-        const days = Math.floor(distance / (1000 * 60 * 60 * 24))
-        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
-        const seconds = Math.floor((distance % (1000 * 60)) / 1000)
-
-        e.innerHTML = `剩餘${days}日${hours}時${minutes}分${seconds}秒`
-      } else {
-        clearInterval(countdownInterval)
-        e.innerHTML = '時間到！'
-      }
-    })
+    if (distance > 0) {
+      setTimer('days', Math.floor(distance / (1000 * 60 * 60 * 24)))
+      setTimer('hours', Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)))
+      setTimer('minutes', Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)))
+      setTimer('seconds', Math.floor((distance % (1000 * 60)) / 1000))
+    } else {
+      clearInterval(countdownInterval);
+      ['days', 'hours', 'minutes', 'seconds'].forEach(r => setTimer(r, 0))
+    }
   }, 1000)
 })
